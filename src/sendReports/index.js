1cb97e8849866f66f5bdb472b68cd35c8b4bd83d
prasannnaghantasala <prasanna.ghantasala@bizcloudexperts.com>
2024-02-22 17:14:55 +0530

    adding the success email

--- src/sendReports/index.js
+++ src/sendReports/index.js
@@ -9,15 +9,8 @@ let functionName = "";
 
 module.exports.handler = async (event, context) => {
   functionName = context.functionName;
-  const executionArn = get(event, "executionArn");
-  const dataLoadedToS3Count = get(event, "dataLoadedToS3");
-
-  // Check if executionArn is missing
-  if (!executionArn) {
-    console.error("executionArn is missing or undefined");
-    return { message: "ExecutionArn is missing or undefined." };
-  }
-
+  const executionArn = get(event, "executionArn");                                          //Getting Current executionArn from event
+  const dataLoadedToS3Count = get(event, "dataLoadedToS3")                           
   const stepFunctionName = executionArn.split(":")[6];
   log.INFO(functionName, "Event:" + JSON.stringify(event));
 
@@ -28,17 +21,17 @@ module.exports.handler = async (event, context) => {
       executionArn
     };
 
-    const data = await stepfunctions.listMapRuns(params).promise();
+    const data = await stepfunctions.listMapRuns(params).promise();                    //Getting 1st mapRunArn using executionArn
     const mapRunArns = data.mapRuns.map(run => get(run, 'mapRunArn'));
     log.INFO(functionName, "mapRunArns" + mapRunArns);
 
     const mapRunArn = mapRunArns[0];
 
-    const executionsData = await stepfunctions.listExecutions({
+    const executionsData = await stepfunctions.listExecutions({                      // Gets all the Execution of 1st mapRunArn
       mapRunArn: mapRunArn,
     }).promise();
 
-    const executionArns = executionsData.executions.map(execution => execution.executionArn);
+    const executionArns = executionsData.executions.map(execution => execution.executionArn);              //From the executions making a array of executionArn from executionsData
     log.INFO(functionName, "Execution ARNs:" + executionArns);
 
     const executionMapRunArns = [];
@@ -47,7 +40,7 @@ module.exports.handler = async (event, context) => {
       const executionParams = { executionArn };
 
       const executionData = await stepfunctions.listMapRuns(executionParams).promise();
-      const mapRunArn = executionData.mapRuns.map(run => get(run, 'mapRunArn'));
+      const mapRunArn = executionData.mapRuns.map(run => get(run, 'mapRunArn'));                 // for each executionArn we are getting mapRunArn's
       if (mapRunArn) {
         executionMapRunArns.push(mapRunArn[0]);
       }
@@ -58,10 +51,11 @@ module.exports.handler = async (event, context) => {
     let succeeded = 0;
     let failed = 0;
 
+
     for (const executionMapRunArn of executionMapRunArns) {
       const mapRunArnparams = { mapRunArn: executionMapRunArn };
 
-      const data = await stepfunctions.describeMapRun(mapRunArnparams).promise();
+      const data = await stepfunctions.describeMapRun(mapRunArnparams).promise();                                // Getting data of mapRunArn's like succeeded,failed,running,pending
       succeeded += get(data, 'executionCounts.succeeded', 0);
       failed += get(data, 'executionCounts.failed', 0);
     }
@@ -70,24 +64,22 @@ module.exports.handler = async (event, context) => {
 
     const today = moment().format('YYYY-MM-DD');
     let snsTopicArn;
-
-    if (failed === 0) {
-      snsTopicArn = SNS_TOPIC_ARN_SUCCESS;
-    } else {
-      snsTopicArn = SNS_TOPIC_ARN_FAILURE;
+    if(failed === 0){
+      snsTopicArn = SNS_TOPIC_ARN_SUCCESS
     }
-
-    const snsparams = {
+    else{
+      snsTopicArn = SNS_TOPIC_ARN_FAILURE
+    }
+    const snsparams = {                                                                                         // Sending mail to Support Team.
       Message: `Hi Team, \n This Report is for Salesforce Datasync Job: \n Step Function Name: ${stepFunctionName} \n Total number of records processed: ${dataLoadedToS3Count} \n Number of successful records : ${succeeded} \n Number of failed records: ${failed}, \n `,
       Subject: `Salesforce Report - ${today}`,
       TopicArn: snsTopicArn,
     };
-
     await sns.publish(snsparams).promise();
 
     return { message: "Reports Sent Successfully. " };
   } catch (error) {
-    console.error("Error while sending reports: ", error, error.stack);
+    console.error("Error while sending reports: ",error, error.stack);
     log.ERROR(functionName, "Error while sending reports: " + error + error.stack, 500);
     throw error;
   }
